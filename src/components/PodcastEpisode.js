e4a04d66819e3aa739660e9b1363d25f438fa3d1
joaosilvalopes <jpgdsl@gmail.com>
2023-03-02 11:09:14 +0000

    feat: Add podcast episode view

--- src/components/PodcastEpisode.js
+++ src/components/PodcastEpisode.js
@@ -1,3 +1,45 @@
-export default () => {
-    return null;
+import { useOutletContext, useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+import Card from "./Card";
+
+const Container = styled.div`
+    width: 70%;
+`;
+
+const EpisodeCard = styled(Card)`
+    padding: 2rem 1rem;
+`;
+
+const EpisodeTitle = styled.h2`
+    font-size: 2rem;
+    font-weight: bold;
+`;
+
+const EpisodeDescription = styled.p`
+    margin-top: 2rem;
+`;
+
+const EpisodePlayer = styled.audio`
+    width: 100%;
+    margin-top: 2rem;
+`;
+
+const PodcastEpisode = () => {
+    const { episodeId } = useParams();
+    const { podcast } = useOutletContext();
+    const episode = podcast.episodesById[episodeId];
+
+    return <Container>
+        <EpisodeCard>
+            <EpisodeTitle>{episode.title}</EpisodeTitle>
+            <EpisodeDescription dangerouslySetInnerHTML={{ __html: episode.description} } />
+            <EpisodePlayer controls>
+                <source src={episode.url} type="audio/mpeg" />
+                Your browser does not support the video tag.
+            </EpisodePlayer>
+        </EpisodeCard>
+    </Container>;
 };
+
+export default PodcastEpisode;
